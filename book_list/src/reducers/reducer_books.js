01f34d984ea5929f30aba2ed6d2d5898f4de91b8
Thomas <toaney@gmail.com>
2018-04-28 11:43:42 -0700

    update book list

--- book_list/src/reducers/reducer_books.js
+++ book_list/src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
     { title: "Javascript: The Good Parts", pages: 101 },
-    { title: "Harry Potter", pages: 39 },
-    { title: "The Dark Tower", pages: 85 },
-    { title: "Eloquent Ruby", pages: 1 }
+    { title: "The Structure and Interpretation of Computer Programs", pages: 39 },
+    { title: "Outliers", pages: 85 },
+    { title: "Don't Make Me Think", pages: 1 }
   ];
 }
